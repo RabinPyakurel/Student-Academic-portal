748ab14ca0c9cafa07d8b3e0205badbfdc01f918
ritikasuwal <ritikasuwal1@gmail.com>
2024-10-08 15:17:12 +0545

    Images and profile icon
    Added more images and completed user profile icon

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,23 @@
- // Get the profile image and dropdown menu elements
- const profileImage = document.getElementById('profileImage');
- const dropdownMenu = document.getElementById('dropdownMenu');
+// Get the profile image and dropdown menu elements
+const profileContainer = document.getElementById("profileContainer");
+const dropdownMenu = document.getElementById("dropdownMenu");
 
- // Toggle the dropdown menu visibility on image click
- profileImage.addEventListener('click', function () {
-   if (dropdownMenu.style.display === 'block') {
-     dropdownMenu.style.display = 'none';  // Hide the menu
-   } else {
-     dropdownMenu.style.display = 'block';  // Show the menu
-   }
- });
+// Toggle the dropdown menu visibility on image click
+profileContainer.addEventListener("click", function () {
+  if (dropdownMenu.style.display === "block") {
+    dropdownMenu.style.display = "none"; // Hide the menu
+  } else {
+    dropdownMenu.style.display = "block"; // Show the menu
+  }
+});
 
- // Close the dropdown if the user clicks outside the menu
- window.onclick = function (event) {
-   if (!event.target.matches('.profile-img')) {
-     dropdownMenu.style.display = 'none';
-   }
- }
+// Close the dropdown if the user clicks outside the menu
+window.onclick = function (event) {
+  if (
+    !event.target.matches(".profile-img") &&
+    !event.target.matches(".dropdown-icon") &&
+    !event.target.matches("#profileContainer")
+  ) {
+    dropdownMenu.style.display = "none";
+  }
+};
