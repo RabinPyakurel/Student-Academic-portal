44089891c0de3a824f4b90a2b99be1aa31096821
Rabin Pyakurel <rabinstar137@gmail.com>
2024-10-18 22:12:18 +0545

    changes css and js line for terms and conditions

--- assets/js/terms.js
+++ assets/js/terms.js
@@ -3,24 +3,24 @@ document.addEventListener('DOMContentLoaded', function () {
     const openModalButton = document.getElementById("openTermsModal");
     const closeModalButton = document.querySelector(".close-btn");
 
-    // Open the modal when the link is clicked
+   
     openModalButton.onclick = function (event) {
-        event.preventDefault(); // Prevent the default link behavior
-        modal.style.display = "flex"; // Change to "flex" to enable centering
-        document.body.style.overflow = "hidden"; // Prevent body scrolling
+        event.preventDefault(); 
+        modal.style.display = "flex"; 
+        document.body.style.overflow = "hidden"; 
     }
 
-    // Close the modal when the close button is clicked
+    
     closeModalButton.onclick = function () {
         modal.style.display = "none";
-        document.body.style.overflow = ""; // Restore body scrolling
+        document.body.style.overflow = ""; 
     }
 
-    // Close the modal when clicking anywhere outside of the modal
+    
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
-            document.body.style.overflow = ""; // Restore body scrolling
+            document.body.style.overflow = ""; 
         }
     }
 });
